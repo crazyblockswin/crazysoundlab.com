3573f5217d184568b6649e4199c6e1ddf1aaa12c
crazyblockswin <wallyjcass@gmail.com>
2026-09-24 15:32:36 -0700

    Add files via upload

--- sound-library.js
+++ sound-library.js
@@ -1633,6 +1633,15 @@ window.CSL_SOUND_LIBRARY = {
       "file": "sounds/music/short-rock1.mp3"
     },
     {
+      "id": "sound-mug35y4d",
+      "category": "mixer-samples",
+      "name": "Scene Builder",
+      "description": "how to use scene builder by adding clips",
+      "icon": "🔊",
+      "filename": "scene-builder.mp3",
+      "file": "sounds/mixer-samples/scene-builder.mp3"
+    },
+    {
       "id": "sound-muf00bey",
       "category": "mixer-samples",
       "name": "Sample of Crazy Lab Story with Voice",
@@ -1640,6 +1649,15 @@ window.CSL_SOUND_LIBRARY = {
       "icon": "🔊",
       "filename": "crazy-lab-story.mp3",
       "file": "sounds/mixer-samples/crazy-lab-story.mp3"
+    },
+    {
+      "id": "sound-mug3h7hv",
+      "category": "mixer-samples",
+      "name": "Breaking News",
+      "description": "Chump breaking news",
+      "icon": "🔊",
+      "filename": "crazy-lab-story-news-chump.mp3",
+      "file": "sounds/mixer-samples/crazy-lab-story-news-chump.mp3"
     },
     {
       "id": "sound-muesqog1",
@@ -1775,24 +1793,6 @@ window.CSL_SOUND_LIBRARY = {
       "icon": "🎤",
       "filename": "Sample-mp3-Crazy-Voice-Sound-Lab-Recording-mixed.mp3",
       "file": "sounds/mixer-samples/Sample-mp3-Crazy-Voice-Sound-Lab-Recording-mixed.mp3"
-    },
-    {
-      "id": "sound-mug35y4d",
-      "category": "mixer-samples",
-      "name": "Scene Builder",
-      "description": "how to use scene builder by adding clips",
-      "icon": "🔊",
-      "filename": "scene-builder.mp3",
-      "file": "sounds/mixer-samples/scene-builder.mp3"
-    },
-    {
-      "id": "sound-mug3h7hv",
-      "category": "mixer-samples",
-      "name": "Breaking News",
-      "description": "Chump breaking news",
-      "icon": "🔊",
-      "filename": "crazy-lab-story-news-chump.mp3",
-      "file": "sounds/mixer-samples/crazy-lab-story-news-chump.mp3"
     }
   ]
 };
